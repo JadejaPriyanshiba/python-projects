bc0105c43a9934b5032522c1bdf0df68e377f226
JadejaPriyanshiba <jadeja.code@gmail.com>
2025-06-16 01:31:31 +0530

    first commit

---
+++ ai_note_maker/main.py
@@ -0,0 +1,56 @@
+import requests
+
+def generate_notes(title):
+    url = "http://localhost:1234/v1/completions"
+
+    system_prompt = (
+        "You are a highly skilled academic note maker. "
+    )
+    get_topics = (
+        f"""
+You are an expert academic assistant.
+
+Given the main topic: "{title}", list all the important sections or subtopics that should be covered to make a complete basic and meduim and general detailed learning.
+List topics in bullet points,
+Do not add extra explanation — just give the clean short few words list.
+"""
+    )
+    get_explanation=(
+        "Generate long, accurate, well-structured, and exam-ready notes with proper formatting, bullet points, and clear explanations.\n\n"
+    )
+    full_prompt = f"{system_prompt} {get_topics}"
+
+    payload = {
+        "prompt": full_prompt,
+        "temperature": 0.7,
+        "max_tokens": 1024,
+        # "stop": ["Title:"],
+    }
+
+    response = requests.post(url, json=payload)
+
+    if response.status_code == 200:
+        return response.json()['choices'][0]['text'].strip()
+    else:
+        raise Exception(f"Error: {response.status_code} - {response.text}")
+
+def parse_bullet_list_to_array(raw_output: str):
+    lines = raw_output.strip().split('\n')
+    items = []
+    for line in lines:
+        line = line.strip()
+        if line.startswith("-"):
+            item = line[1:].strip()
+            if item:
+                items.append(item)
+    return items
+
+# === Run ===
+if __name__ == "__main__":
+    title = input("Enter your topic title: ")
+    topics = generate_notes(title)
+    print("\n--- Generated Notes ---\n")
+    topics = parse_bullet_list_to_array(topics)
+    for i in topics :
+        print(str(topics.index(i))+ ".) "+i) 
+
